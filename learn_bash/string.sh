e45462f868d156ffe4e2fb3f0d151aa5a74a6c02
nichel <nichelnich@gmail.com>
2022-07-15 10:27:49 +0800

    Update string.sh

--- learn_bash/string.sh
+++ learn_bash/string.sh
@@ -7,3 +7,21 @@
 2. use tr
 tr 指令从标准输入设备读取数据，经过字符串转译后，将结果输出到标准输出设备。
 
+
+3. match substring
+if [[ "$value" == "cellular_"* ]];then
+ echo match
+fi
+
+4. push string to arry
+service_array=()
+service_array+=("$value")
+
+5. split string to arry
+IFS='.' read -r -a myarray <<< "$1"
+
+6. read string line by line
+while read var; do
+ echo $var
+done <<< $service_info
+#done < test.txt

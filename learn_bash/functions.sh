5dd8a421be0ad5a6e325d11300fb7729b1d11ef3
nichel <nichelnich@gmail.com>
2022-09-02 14:40:37 +0800

    Update functions.sh

--- learn_bash/functions.sh
+++ learn_bash/functions.sh
@@ -7,3 +7,15 @@ check_ip() {
     fi
     return 1
 }
+
+check_mac() {
+    case $1
+        in
+            [0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F])
+            return 0
+        ;;
+        *)  
+            return 1
+        ;;
+    esac
+}
